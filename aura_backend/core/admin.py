46a617f0cee0cf5126f5dd1735d37f892fdba7b0
reyesjl <reyesjl.nine@hotmail.com>
2025-05-22 17:52:15 -0400

    No Story - add models to admin screen

--- aura_backend/core/admin.py
+++ aura_backend/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from core.models import Carrier, CoverageLine, Question, ApplicationSession, Submission
 
-# Register your models here.
+admin.site.register(Carrier)
+admin.site.register(CoverageLine)
+admin.site.register(Question)
+admin.site.register(ApplicationSession)
+admin.site.register(Submission)
