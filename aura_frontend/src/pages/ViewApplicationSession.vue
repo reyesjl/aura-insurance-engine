41e8f2b4c47e17eb5d9c26f2ff1d92b085f8ef53
reyesjl <reyesjl.nine@hotmail.com>
2025-07-23 16:43:11 -0400

    No Story - update token

--- aura_frontend/src/pages/ViewApplicationSession.vue
+++ aura_frontend/src/pages/ViewApplicationSession.vue
@@ -165,9 +165,12 @@ const obfuscatedToken = computed(() => {
 })
 
 function rollToken() {
-  const uuid = ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-    (Number(c) ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> Number(c) / 4).toString(16)
-  )
+  // Proper UUID v4 generator
+  const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c: string) => {
+    const r = crypto.getRandomValues(new Uint8Array(1))[0] % 16
+    const v = c === 'x' ? r : (r & 0x3 | 0x8)
+    return v.toString(16)
+  })
   console.log('Rolled new token:', uuid)
   tokenRefreshed.value = true
   if (refreshTimeout) clearTimeout(refreshTimeout)
